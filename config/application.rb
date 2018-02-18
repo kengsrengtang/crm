ba0b6f4cdaddca97073e829da90d46b6319cfe9c
kengtang <kengtang@cisco.com>
2018-02-18 14:24:49 -0800

    added factory bot gem

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Crm
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.factory_bot false
+    end
   end
 end
